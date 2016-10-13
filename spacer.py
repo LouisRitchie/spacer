bd792e3c4ea30038c4f783f5379dd6740d872fdd
Louis Ritchie <louiscritchie@gmail.com>
2016-10-12 17:08:45 -0700

    End brace will be on lower level than prev lines

--- spacer.py
+++ spacer.py
@@ -10,11 +10,12 @@ lastLine = ""
 for line in file1:
   if len(line) == 0:
     continue
+  if '}' in line and counter >= 0:
+    counter -= 1
   output += "  " * counter + line.replace("\n", "").replace("  ", "") + "\n"
   if '{' in line:
     counter += 1
-  if '}' in line and counter >= 0:
-    counter-= 1
+
 if output[-1] != "\n":
   output += "\n"
 
