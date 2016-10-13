99273b0643ebb71fa037e363564d507e1c061b88
Louis Ritchie <louiscritchie@gmail.com>
2016-10-13 14:51:05 -0700

    Add a space under a closing brace

--- spacer.py
+++ spacer.py
@@ -8,13 +8,17 @@ output = ""
 lastLine = ""
 
 for line in file1:
+  addNewline = False
   if line.isspace():
     continue
   if '}' in line and counter >= 0:
     counter -= 1
+    addNewline = True
   output += "  " * counter + line.replace("\n", "").replace("  ", "") + "\n"
   if '{' in line:
     counter += 1
+  if addNewline:
+    output += "\n"
 
 if output[-1] != "\n":
   output += "\n"
